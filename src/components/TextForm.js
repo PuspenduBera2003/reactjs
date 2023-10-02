f701784b9797db223a8bca194265ba4bec0e4d8f
Puspendu <puspendubera28@gmail.com>
2023-10-02 16:00:10 +0530

    02-10-2023 16:00

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
-    const handleUpClick = () =>{
+    const handleUpClick = () => {
         const uppercase_string = text.toUpperCase();
         setText(uppercase_string);
     }
 
-    const handleLowClick = () =>{
+    const handleLowClick = () => {
         const lowercase_string = text.toLowerCase();
         setText(lowercase_string);
     }
@@ -18,7 +18,7 @@ export default function TextForm(props) {
 
     const toggleText = () => {
         let text_ip = document.getElementById('text_input');
-        if(text_ip.style.display === "none" && text.length !== 0) {
+        if (text_ip.style.display === "none" && text.length !== 0) {
             text_ip.style.display = 'block';
         }
         else {
@@ -26,28 +26,71 @@ export default function TextForm(props) {
         }
     }
 
+    const clearText = () => {
+        setText('');
+    }
+
+    const backspace = ()=> {
+        setText(text.slice(0,-1));
+    }
+
+    const textWords = () => {
+        if (text.length === 0) {
+            return 0;
+        }
+        else {
+            return text.split(/\s+/).filter(word => word !== "").length;
+        }
+    }
+
     const [text, setText] = useState('');
+
+    let wordCount = textWords();
+
+    const timeCalculation = ()=> {
+        if(wordCount*0.17 > 3600) {
+            let wordCountminute = wordCount*0.17/3600;
+            return (wordCountminute+" hours");
+        }
+        else if(wordCount*0.17 > 60) {
+            let wordCountminute = wordCount*0.17/60;
+            return (wordCountminute+" minutes");
+        }
+        else {
+            let wordCountsecond = wordCount*0.17;
+            return (wordCountsecond+" seconds");
+        }
+    }
+
+    let requiredTime = timeCalculation();
 
     return (
         <div className='container my-5'>
             <h1 className="card-title text-center">{props.heading}</h1>
             <div className="form-floating m-auto my-4">
-                <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" name="input" value={text} onChange={handleOnChange} style={{height: "200px"}}></textarea>
+                <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" name="input" value={text} onChange={handleOnChange} style={{ height: "200px" }}></textarea>
                 <label htmlFor="floatingTextarea2">Enter Your Text Here</label>
-                <button className="btn btn-dark mt-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-dark mt-2" onClick={backspace}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-backspace-fill" viewBox="0 0 16 16">
+                        <path d="M15.683 3a2 2 0 0 0-2-2h-7.08a2 2 0 0 0-1.519.698L.241 7.35a1 1 0 0 0 0 1.302l4.843 5.65A2 2 0 0 0 6.603 15h7.08a2 2 0 0 0 2-2V3zM5.829 5.854a.5.5 0 1 1 .707-.708l2.147 2.147 2.146-2.147a.5.5 0 1 1 .707.708L9.39 8l2.146 2.146a.5.5 0 0 1-.707.708L8.683 8.707l-2.147 2.147a.5.5 0 0 1-.707-.708L7.976 8 5.829 5.854z" />
+                    </svg>
+                </button>
+                <button className="btn btn-dark mt-2 ms-3" onClick={clearText}>Clear Text</button>
+                <button className="btn btn-dark mt-2 ms-3" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-dark mt-2 ms-3" onClick={handleLowClick}>Convert to Lowercase</button>
-                <button className="btn btn-dark mt-2 ms-3" onClick={toggleText}>Toggle View/Hide</button>
-            </div>
-            <div className="container mb-3" id='text_input' style={{display: 'none'}}>
-                <h4>Your text is</h4>
-                <p className="card-text" style={{textAlign: "justify"}}>
-                    {text}
-                </p>
+                <button className="btn btn-dark mt-2 ms-3" onClick={toggleText}>Toggle Preview</button>
             </div>
             <div className="container">
-                <h4>Summary of your text is</h4>
-                <p className="card-text" style={{textAlign: "justify"}}>
-                    {text.split(" ").length} words and {text.length} characters
+                <h4>Your text summary</h4>
+                <p className="card-text" style={{ textAlign: "justify", lineHeight: "5vh" }}>
+                    {wordCount} words and {text.length} characters <br />
+                    You required {requiredTime} time to read this text.
+                </p>
+            </div>
+            <div className="container mt-3" id='text_input' style={{ display: 'none' }}>
+                <h4>Preview</h4>
+                <p className="card-text" style={{ textAlign: "justify" }}>
+                    {text}
                 </p>
             </div>
         </div>
@@ -55,5 +98,5 @@ export default function TextForm(props) {
 }
 
 TextForm.propTypes = {
-    heading : PropTypes.string,
+    heading: PropTypes.string,
 }
